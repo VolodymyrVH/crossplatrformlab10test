3d5df71614af6be6b67dfc98e37200826bbd742e
VolodymyrVH <gorelovv515@gmail.com>
2025-11-27 23:48:18 +0100

    Deploy Flutter web with relative paths

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1898381807"
+    serviceWorkerVersion: "866351926"
   }
 });
